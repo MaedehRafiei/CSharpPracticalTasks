d62f2d6a82f607db70089daaeca7a6479c470ab9
m <mm>
2025-12-04 23:47:28 +0330

    Add project files.

---
+++ ConsoleApp1/Number.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class Number
+    {
+        public static List<int> FilterNumbers(List<int> numbers)
+        {
+            if (numbers == null)
+                throw new ArgumentNullException("numbers is null");
+
+            return numbers.Where(n => n > 5).ToList();
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter numbers");
+            var input = Console.ReadLine();
+            var numbers = input.Split(new char[] { '-' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            var result = Number.FilterNumbers(numbers);
+            Console.WriteLine("numbers Greater Than 5 is : " + string.Join(", ", result));
+            Console.ReadLine();
+
+        }
+    }
+}
